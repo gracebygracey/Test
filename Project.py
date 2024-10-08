a03ac48e80777759a8d012573b002f01f12d8d20
gracebygracey <ngraceyseven@gmail.com>
2024-10-08 09:52:05 +0545

    test file

---
+++ Project.py
@@ -0,0 +1,12 @@
+# defining my function
+def my_function(fname, lname):
+    print(fname + " " + lname)
+# calling my function
+my_function("gracey", "madhavi")
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
